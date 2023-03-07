72efec01604507598bea3b6b3273cdcb24e0a05f
NazarenkoDarina <nazarenkodarina@yandex.ru>
2023-03-07 13:14:08 +0500

    HelloWorldLab

---
+++ app/src/main/java/com/example/helloworldlab/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.helloworldlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.view.View
+import android.view.View.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun OnClickShowButton(view: View){
+        val editText=findViewById<TextView>(R.id.editTextTextMultiLine)
+        val showText=findViewById<TextView>(R.id.textView)
+        showText.text=editText.text.toString()
+    }
+}
